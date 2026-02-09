45bf5b4d534bfdfe5ecff8763ce6a6881caa0560
Vib-D <vibhordass2@gmail.com>
2026-02-09 22:31:39 +0530

    LoginTest browser setup

--- playwright-automation/src/test/java/testcases/LoginTest.java
+++ playwright-automation/src/test/java/testcases/LoginTest.java
@@ -1,6 +1,17 @@
 package testcases;
+
+
+import org.testng.annotations.Test;
+
+
 import base.BaseTest;
 
 public class LoginTest extends BaseTest {
-    
+
+    @Test
+    public void doLogin(){
+        getBrowser("chrome");
+        navigate(browser, "https://google.com");
+    }
+
 }
